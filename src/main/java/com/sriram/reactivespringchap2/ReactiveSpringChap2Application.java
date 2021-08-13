4e847892d4b1f0b355b0edcfae8688b17891aab9
Prashanth <prashanthsriram145@gmail.com>
2021-08-13 20:34:12 +0530

    initial commit

---
+++ src/main/java/com/sriram/reactivespringchap2/ReactiveSpringChap2Application.java
@@ -0,0 +1,13 @@
+package com.sriram.reactivespringchap2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveSpringChap2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveSpringChap2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/sriram/reactivespringchap2/ReactiveSpringChap2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sriram.reactivespringchap2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveSpringChap2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
